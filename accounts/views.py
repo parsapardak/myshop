4e976e4a1e6ffaba782403f77e6027b81be31191
parsapardak <parsa.pardak@gmail.com>
2025-02-14 20:00:32 +0330

    Create signup view for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('products:product_list')  # یا هر صفحه دلخواهی
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
